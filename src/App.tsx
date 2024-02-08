5ecf302f7daa3be3395f55e433e46938bad39bab
lovers2020 <dkrkskswl@naver.com>
2024-02-08 21:43:27 +0900

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Layout from "./components/layout";
 import NotFound from "./components/notfound";
 import Home from "./home";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        errorElement: <NotFound />,
-        children: [
-            {
-                path: "",
-                element: <Home />,
-            },
-        ],
-    },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <Layout />,
+            errorElement: <NotFound />,
+            children: [
+                {
+                    path: "",
+                    element: <Home />,
+                },
+            ],
+        },
+    ],
+    { basename: "/iom-landingpage" }
+);
 function App() {
     return <RouterProvider router={router}></RouterProvider>;
 }
